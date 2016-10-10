e4c8a6d6e385337ff545a221ad80c9ca50eb19dc
Николай Дымура <kolyav1ampir@gmail.com>
2016-10-10 15:35:35 +0300

    js to add new block

--- js/create_block.js
+++ js/create_block.js
@@ -1,5 +1,28 @@
 $(function (){
 	$('.create_block').click(function(){
-    $('.divs').append('<p>Меня тут не было!</p>');
-})
+		var object_name = $('#object_name').val();
+		var adress = $('#adress').val();
+		var contacts = $('#contacts').val();
+		var information = $('#information').val();
+    	$('.basic').append('<div class="object1">'+
+       ' <img alt="close button" src="style/png/close.png" class="clear_coment">'+
+            '<div class="img">'+
+                '<img src="style/png/No_img.png" alt="picture">'+
+            '</div>'+
+            '<div class="object_name">'+
+                object_name +
+            '</div>'+
+            '<br>'+
+            '<div class="adress">'+
+                'Адрес: ' +adress +
+            '</div>'+
+            '<div class="contacts">'+
+                'Номер телефона: '+ contacts +
+            '</div>'+
+            '<br>'+
+            '<div class="information">'+
+                 information +
+            '</div>'+
+        '</div>');
+	})
 });
